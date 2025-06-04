d2d3624958552351dbf9c744b16248ece7be47df
sunyiqiu <221850015@smail.nju.edu.cn>
2025-06-04 14:28:01 +0800

    fix: merge错误

--- service/question_service.py
+++ service/question_service.py
@@ -222,19 +222,19 @@ def __generate_references(entries : list[Entry], preProcessResult : PreProcessRe
         if (match := __fuzzy_match(query=mod_name, candidates=global_vars.all_mod_names, threshold=80)) is not None
     ]
     std_item_names = [
-        match for item_name in preProcessResult.extraction_fields.items
+        match for item_name in preProcessResult.extracted_fields.items
         if (match := __fuzzy_match(query=item_name, candidates=global_vars.all_item_names, threshold=90)) is not None
     ]
     std_boime_names = [
-        match for boime_name in preProcessResult.extraction_fields.biomes
+        match for boime_name in preProcessResult.extracted_fields.biomes
         if (match := __fuzzy_match(query=boime_name, candidates=global_vars.all_biome_names, threshold=90)) is not None
     ]
     std_entity_names = [
-        match for entity_name in preProcessResult.extraction_fields.entities
+        match for entity_name in preProcessResult.extracted_fields.entities
         if (match := __fuzzy_match(query=entity_name, candidates=global_vars.all_entity_names, threshold=90)) is not None
     ]
     std_structure_names = [
-        match for structure_name in preProcessResult.extraction_fields.structures
+        match for structure_name in preProcessResult.extracted_fields.structures
         if (match := __fuzzy_match(query=structure_name, candidates=global_vars.all_structure_names, threshold=90)) is not None
     ]
     logger.info(f"std_names_matched: mod={std_mod_names}, item={std_item_names}, biome={std_boime_names}, entity={std_entity_names}, structure={std_structure_names}")
